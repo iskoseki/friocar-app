7427ad0be65d44c09ca0e32e17541b50f1ecd5c7
kosekijsx <leandrobordondev@gmail.com>
2023-09-11 14:34:07 -0300

    Home building 👷🏼‍♀️  ️

--- src/app/components/Carrousel.js
+++ src/app/components/Carrousel.js
@@ -28,18 +28,16 @@ export default function Carrousel() {
             <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
               <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                  Example Title
+                  Servicio
                   <br class="hidden lg:inline-block" />
-                  Number 1
+                  Calefacción{" "}
                 </h1>
                 <p class="mb-8 leading-relaxed">
-                  Copper mug try-hard pitchfork pour-over freegan heirloom
-                  neutra air plant cold-pressed tacos poke beard tote bag.
-                  Heirloom echo park mlkshk tote bag selvage hot chicken
-                  authentic tumeric truffaut hexagon try-hard chambray.
+                  Reparamos todo tipo de calefacción en vehículos. Revisamos
+                  fallas por computadora. Contamos con amplia experiencia.
                 </p>
                 <div class="flex justify-center">
-                  <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  <button class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
                     Button
                   </button>
                   <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
@@ -62,18 +60,49 @@ export default function Carrousel() {
             <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
               <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                  Example Title
+                  Direcciones
                   <br class="hidden lg:inline-block" />
-                  Number 2
+                  hidráulicas
                 </h1>
                 <p class="mb-8 leading-relaxed">
-                  Copper mug try-hard pitchfork pour-over freegan heirloom
-                  neutra air plant cold-pressed tacos poke beard tote bag.
-                  Heirloom echo park mlkshk tote bag selvage hot chicken
-                  authentic tumeric truffaut hexagon try-hard chambray.
+                  Reparación de direcciones hidráulicas en vehículos de toda
+                  gama.
                 </p>
                 <div class="flex justify-center">
-                  <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  <button class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
+                    Button
+                  </button>
+                  <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                    Button
+                  </button>
+                </div>
+              </div>
+              <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <img
+                  class="object-cover object-center rounded"
+                  alt="hero"
+                  src="https://dummyimage.com/720x600"
+                />
+              </div>
+            </div>
+          </section>
+        </SwiperSlide>
+        <SwiperSlide>
+          <section class="text-gray-600 body-font">
+            <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+              <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                  Servicio de
+                  <br class="hidden lg:inline-block" />
+                  Tren delantero
+                </h1>
+                <p class="mb-8 leading-relaxed">
+                  Realizamos una revisión y diagnóstico del tren delantero de tu
+                  vehículo, para luego desarrollar el trabajo de reparación en
+                  forma certera.
+                </p>
+                <div class="flex justify-center">
+                  <button class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
                     Button
                   </button>
                   <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
--- src/app/components/cta.js
+++ src/app/components/cta.js
@@ -4,7 +4,7 @@ function Cta() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <svg
             viewBox="0 0 1024 1024"
             className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -20,7 +20,7 @@ function Cta() {
             <defs>
               <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
                 <stop stopColor="#7775D6" />
-                <stop offset={1} stopColor="#E935C1" />
+                <stop offset={1} stopColor="#FF2200" />
               </radialGradient>
             </defs>
           </svg>
@@ -52,7 +52,7 @@ function Cta() {
           <div className="relative mt-16 h-80 lg:mt-8">
             <img
               className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-              src="https://dummyimage.com/1824x1080/000/fff"
+              src="https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2832&q=80"
               alt="App screenshot"
               width={1824}
               height={1080}
