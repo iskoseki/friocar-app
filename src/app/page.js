8629d5e2aa89217f6e6be7078f5b938f1d8224b9
kosekijsx <leandrobordondev@gmail.com>
2023-09-16 12:56:08 -0300

    Fix First deploy

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Carrousel from "./components/Carrousel";
 import Testimonials from "./components/Testimonials";
 import Cta from "./components/cta";
 import SimpleCta from "./components/SimpleCta";
-import Porquenos from "./components/porqueNos";
+import Porquenos from "./components/Porquenos.jsx";
 
 export default function Home() {
   return (
