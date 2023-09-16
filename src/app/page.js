024d08a0edc300f452748247feb763a2c03c90e0
kosekijsx <leandrobordondev@gmail.com>
2023-09-16 13:45:43 -0300

    Fix Function deploy

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Carrousel from "./components/Carrousel";
 import Testimonials from "./components/Testimonials";
 import Cta from "./components/cta";
 import SimpleCta from "./components/SimpleCta";
-import Porquenos from "./components/Porquenos.jsx";
+import Porquenos from "./components/whyus";
 
 export default function Home() {
   return (
